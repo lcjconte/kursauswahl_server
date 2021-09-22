2aa8ffd4188044e4d2e458c2f62bb5204f74305b
Lcj <lcjconte@gmail.com>
2021-09-22 20:28:44 +0200

    Added welcome txt

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 
 #[macro_use] extern crate rocket;
 
+use std::path::Path;
+use rocket::{response, fs::NamedFile};
 
 #[get("/")]
-fn index() -> &'static str {
-  "This is my Rocket Demo app"
+fn index() -> response::Redirect {
+  response::Redirect::to(uri!("/welcome"))
+}
+
+#[get("/welcome")]
+async fn welcome() -> Option<NamedFile>{
+  let path = Path::new("./client/files/welcome.txt");
+  NamedFile::open(path).await.ok()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, welcome])
 }
